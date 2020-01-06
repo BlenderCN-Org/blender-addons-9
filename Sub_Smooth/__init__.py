60940f1615bd98936c9318676bb8fba36312b5e0
FlashBlaze <neerajlagwankar@gmail.com>
2020-01-06 22:33:07 +0530

    Added boilerplate to get started, thanks CG Cookie 🙌

--- Sub_Smooth/__init__.py
+++ Sub_Smooth/__init__.py
@@ -14,18 +14,37 @@
 bl_info = {
     "name": "Sub Smooth",
     "author": "Neeraj Lagwankar",
-    "description": "",
+    "description": "A simple addon to subdivide and smooth the selected object",
     "blender": (2, 80, 0),
     "version": (0, 0, 1),
-    "location": "",
+    "location": " View3D > Object",
     "warning": "",
     "category": "Generic"
 }
 
+import bpy
+from bpy.types import (Operator)
+
+
+# Since it's an Object operator, the naming convention for class is:
+# OBJECT_OT_lower_case
+
+class OBJECT_OT_subsmooth(Operator):
+    bl_label = "SubSmooth"
+    bl_idname = "object.subsmooth"
+    bl_description = "A simple addon to subdivide and smooth the selected object"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_options = {"REGISTER", "UNDO"}
+
 
 def register():
-    ...
+    bpy.utils.register_class(OBJECT_OT_subsmooth)
 
 
 def unregister():
-    ...
+    bpy.utils.unregister_class(OBJECT_OT_subsmooth)
+
+
+if __name__ == "__main__":
+    register()
